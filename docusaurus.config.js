01e8d51bb0d5fc9e662cb771245d5e3332ae595d
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2026-02-20 13:28:00 +0530

    added google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,6 +69,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-QMMNM743FK',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
