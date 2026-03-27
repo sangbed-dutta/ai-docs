a24ea8588979c413832c150a79e7cf72e4530dc4
Tiwariji-07 <vivek.raj@wavemaker.com>
2026-03-27 11:29:22 +0530

    prod agent url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
 
   customFields: {
     ecosystemAgentUrl:
-      process.env.WAVEMAKER_AGENT_URL || 'http://54.147.142.76:8069',
+      process.env.WAVEMAKER_AGENT_URL || 'https://ecosystem-agent.wavemaker.ai',
   },
 
   presets: [
